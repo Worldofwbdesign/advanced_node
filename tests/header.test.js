317ee9b31784da7557bbc4550f670079d9151cd9
Roman Martynenko <rmartynenko@deloitte.com>
2018-10-19 09:14:04 +0300

    Added tests

---
+++ tests/header.test.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  page = await browser.newPage();
+
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test('Should show correct text in header', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(text).toEqual('Blogster');
+});
+
+test('Should go to google accounts', async () => {
+  await page.click('.right a');
+
+  const url = page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
